3272d033f0004dc7286ba6d53cbad2038da37481
Kajal <fhkajal.ouritspace@gmail.com>
2025-12-28 10:20:12 +0600

    fix

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -9,7 +9,7 @@ export const baseApi = createApi({
     // baseUrl: "https://geniustutorss-backend.vercel.app/api",
     // baseUrl: "http://localhost:5008/api",
     // baseUrl: "http://160.25.7.224:5000/api",
-    baseUrl: "https://www.geniustutorss.com/api/api",
+    baseUrl: "https://geniustutorss.com/api/api",
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
